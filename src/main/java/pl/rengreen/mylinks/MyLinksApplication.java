7e629284de1382199dbfb1da8c161ce9e9914bcf
rengreen <r.m.gardzinska@gmail.com>
2019-01-26 17:34:13 +0100

    Spring Initializr

---
+++ src/main/java/pl/rengreen/mylinks/MyLinksApplication.java
@@ -0,0 +1,15 @@
+package pl.rengreen.mylinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLinksApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MyLinksApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/rengreen/mylinks/MyLinksApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.rengreen.mylinks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyLinksApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
