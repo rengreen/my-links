6bfbbfb41f97535910b13fa9ac08f2ef2cd91790
rengreen <r.m.gardzinska@gmail.com>
2019-01-27 23:57:46 +0100

    adding more initial data and logger message

--- src/main/java/pl/rengreen/mylinks/DataLoader.java
+++ src/main/java/pl/rengreen/mylinks/DataLoader.java
@@ -10,6 +10,7 @@ import pl.rengreen.mylinks.repository.LinkRepository;
 @Service
 public class DataLoader implements CommandLineRunner {
 
+    private final Logger logger = LoggerFactory.getLogger(DataLoader.class);
     private LinkRepository linkRepository;
 
     public DataLoader(LinkRepository linkRepository) {
@@ -19,5 +20,9 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         linkRepository.save(new Link("Spring Initializr","https://start.spring.io/","spring img","Java"));
+        linkRepository.save(new Link("Google Java Style Guide","https://google.github.io/styleguide/javaguide.html","guide img","Java"));
+        linkRepository.save(new Link("Virus Total","https://www.virustotal.com/#/home/upload","virus img","Security"));
+        linkRepository.save(new Link("empty","empty","empty","empty"));
+        logger.info("Inserting initial data into DATABASE.");
     }
 }
