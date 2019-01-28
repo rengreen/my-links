dca9cedaf4e25aef6663efbdc33b02ac5029253c
rengreen <r.m.gardzinska@gmail.com>
2019-01-28 21:07:45 +0100

    adiing CRUD methods to service interface

--- src/main/java/pl/rengreen/mylinks/service/LinkService.java
+++ src/main/java/pl/rengreen/mylinks/service/LinkService.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 public interface LinkService {
     public abstract List<Link> getAllLinks();
+    public abstract Link getLinkById(long id);
+    public abstract void saveLink(Link link);
+    public abstract void updateLink(Link link);
+    public abstract void deleteLink(long id);
 }
 
 
