effc02822b6dabbe718194650a9312b19ac1f734
rengreen <r.m.gardzinska@gmail.com>
2019-01-28 20:33:45 +0100

    changing CrudRepository to JpaRepository

--- src/main/java/pl/rengreen/mylinks/repository/LinkRepository.java
+++ src/main/java/pl/rengreen/mylinks/repository/LinkRepository.java
@@ -1,8 +1,8 @@
 package pl.rengreen.mylinks.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.rengreen.mylinks.model.Link;
 
-public interface LinkRepository extends CrudRepository <Link, Long> {
+public interface LinkRepository extends JpaRepository<Link, Long> {
 
 }
