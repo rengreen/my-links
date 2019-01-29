23b26b158d4ef45246bd0713bd19b26b768701b3
rengreen <r.m.gardzinska@gmail.com>
2019-01-29 21:15:02 +0100

    switching initial data source to JSON file

--- src/main/java/pl/rengreen/mylinks/DataLoader.java
+++ src/main/java/pl/rengreen/mylinks/DataLoader.java
@@ -38,13 +38,20 @@ public class DataLoader implements CommandLineRunner {
             JSONObject jsonObject = (JSONObject) object;
 
             String name = (String) jsonObject.get("name");
-            System.out.println("name=" + name);
+            String linkUrl = (String) jsonObject.get("linkUrl");
+            String imageUrl = (String) jsonObject.get("imageUrl");
+            String category = (String) jsonObject.get("category");
+
+            linkRepository.save(new Link(name, linkUrl, imageUrl, category));
         }
 
+        /*
         linkRepository.save(new Link("Spring Initializr", "https://start.spring.io/", "spring img", "Java"));
         linkRepository.save(new Link("Google Java Style Guide", "https://google.github.io/styleguide/javaguide.html", "guide img", "Java"));
         linkRepository.save(new Link("Virus Total", "https://www.virustotal.com/#/home/upload", "virus img", "Security"));
         linkRepository.save(new Link("empty", "empty", "empty", "empty"));
+        */
+
         logger.info("Initial data inserted to DATABASE.");
     }
 }
