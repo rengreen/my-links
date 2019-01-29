e95fdeb7af41746bbe81623ddcc596115a8bf068
rengreen <r.m.gardzinska@gmail.com>
2019-01-29 21:08:24 +0100

    adding JSON file with initial db content

--- src/main/java/pl/rengreen/mylinks/DataLoader.java
+++ src/main/java/pl/rengreen/mylinks/DataLoader.java
@@ -1,5 +1,6 @@
 package pl.rengreen.mylinks;
 
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,22 +8,43 @@ import org.springframework.stereotype.Service;
 import pl.rengreen.mylinks.model.Link;
 import pl.rengreen.mylinks.repository.LinkRepository;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 @Service
 public class DataLoader implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(DataLoader.class);
     private LinkRepository linkRepository;
 
-    public DataLoader(LinkRepository linkRepository) {
+    public DataLoader(LinkRepository linkRepository) throws FileNotFoundException, IOException, ParseException {
         this.linkRepository = linkRepository;
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        linkRepository.save(new Link("Spring Initializr","https://start.spring.io/","spring img","Java"));
-        linkRepository.save(new Link("Google Java Style Guide","https://google.github.io/styleguide/javaguide.html","guide img","Java"));
-        linkRepository.save(new Link("Virus Total","https://www.virustotal.com/#/home/upload","virus img","Security"));
-        linkRepository.save(new Link("empty","empty","empty","empty"));
-        logger.info("Inserting initial data into DATABASE.");
+
+        String fileName = "src/main/resources/json/start-data.json";
+        JSONParser parser = new JSONParser();
+        JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(fileName));
+
+        for (int i = 0; i < jsonArray.size(); ++i) {
+            Object object = jsonArray.get(i);
+            JSONObject jsonObject = (JSONObject) object;
+
+            String name = (String) jsonObject.get("name");
+            System.out.println("name=" + name);
+        }
+
+        linkRepository.save(new Link("Spring Initializr", "https://start.spring.io/", "spring img", "Java"));
+        linkRepository.save(new Link("Google Java Style Guide", "https://google.github.io/styleguide/javaguide.html", "guide img", "Java"));
+        linkRepository.save(new Link("Virus Total", "https://www.virustotal.com/#/home/upload", "virus img", "Security"));
+        linkRepository.save(new Link("empty", "empty", "empty", "empty"));
+        logger.info("Initial data inserted to DATABASE.");
     }
 }
